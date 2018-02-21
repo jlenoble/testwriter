3240f217aeb33f6ad3879049a147bb86d8fa3b95
Jason Lenoble <jason.lenoble@gmail.com>
2018-02-21 16:11:47 +0100

    Sanety check on number of descriptions

--- src/runnermap.js
+++ src/runnermap.js
@@ -1,7 +1,11 @@
-import {descend} from './utils';
+import {descend, count} from './utils';
 
 export default class RunnerMap {
   constructor (descriptions) {
+    if (!count(descriptions)) {
+      throw new Error('No descriptions provided for tests');
+    }
+
     Object.defineProperty(this, 'descriptions', {
       value: descriptions,
       enumerable: true,
